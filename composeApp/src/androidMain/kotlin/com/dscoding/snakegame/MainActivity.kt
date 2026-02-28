6ce96d400438be0c05145d17bc81ca554f8c5035
diogocastrosantos <diogocastrosantos@gmail.com>
2026-02-28 19:24:54 +0000

    Add Orientation Lock

--- composeApp/src/androidMain/kotlin/com/dscoding/snakegame/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/dscoding/snakegame/MainActivity.kt
@@ -10,6 +10,7 @@ import com.dscoding.snakegame.app.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
